087750efa7da3cbbe7c6149a39bdce38ab80863b
manan-m-shah <shahmananvyara@gmail.com>
2023-04-08 18:44:42 +0530

    🚀 Working Installation

--- bin/joks.js
+++ bin/joks.js
@@ -5,38 +5,33 @@ import add from "../lib/commands/add.js";
 import check from "../lib/commands/check.js";
 import remove from "../lib/commands/remove.js";
 
-program.version('0.1.0');
+program.version("0.1.0");
 
 program
-  .command('add <package>')
-  .alias('i')
-  .alias('install')
-  .alias('get')
-  .description('add a package')
-  .action(packageName => {
-    add(packageName)
+  .command("add <package>")
+  .alias("i")
+  .alias("install")
+  .alias("get")
+  .description("add a package")
+  .action((packageName) => {
+    add(packageName);
   });
 
 program
-  .command('remove <package>')
-  .alias('rm')
-  .alias('uninstall')
-  .alias('delete')
-  .description('remove a package')
-  .action(packageName => {
-    remove(packageName)
+  .command("remove <package>")
+  .alias("rm")
+  .alias("uninstall")
+  .alias("delete")
+  .description("remove a package")
+  .action((packageName) => {
+    remove(packageName);
   });
 
 program
-    .command('check <package>')
-    .description('check if a package is installed')
-    .action(packageName => {
-        // implementation for checking if a package is installed
-        if (check(packageName)) {
-            console.log(`${packageName} is installed!`);
-        } else {
-            console.log(`${packageName} is not installed!`);
-        }
-    });
+  .command("check <package>")
+  .description("check if a package is installed")
+  .action((packageName) => {
+    check(packageName);
+  });
 
 program.parse(process.argv);
